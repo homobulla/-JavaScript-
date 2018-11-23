dff0aae0fec15b29e804b962d321bcc383e80a51
homobulla <homobulla@outlook.com>
2018-11-23 16:07:50 +0800

    docs: <字典和散列表>

--- 字典和散列表/hashTable.js
+++ 字典和散列表/hashTable.js
@@ -1,1 +1,35 @@
 // 散列表
+
+function HashTable() {
+    let table = {}
+
+    const loseloseHashCode = function(key) {
+        let hash = 0
+        key.split('').map((v, i) => {
+            hash += key.charCodeAt(i)
+        })
+        return hash % 37
+    }
+
+    this.put = function(key, value) {
+        let position = loseloseHashCode(key)
+        console.log(position + ' - ' + key)
+        table[position] = value
+    }
+
+    this.get = function(key) {
+        return table[loseloseHashCode(key)]
+    }
+
+    this.remove = function(key) {
+        table[loseloseHashCode(key)] = undefined
+    }
+}
+
+let hash = new HashTable()
+hash.put('Gandalf', 'gandalf@email.com')
+hash.put('John', 'johnsnow@email.com')
+hash.put('John', 'sssssssssssss')
+
+hash.put('Tyrion', 'tyrion@email.com')
+console.log(hash.get('John'))
